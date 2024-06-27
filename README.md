314c99393495cff893ab3defec22466b2add17bb
Suman Saha <ssaha4@br013.ib.bridges2.psc.edu>
2024-06-27 09:30:05 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# QMS 2024 - day 5
+
+PI: Francesco Paesani 
+Team UCSD: Suman Saha, Vibhu Vardhan Singh
---
+++ ion_water/analyse_traj/sub.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name="mbpol_bulk"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8 
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=RM-small
+#SBATCH --time=00:05:00
+#SBATCH --output="stdout"
+#SBATCH --error="stderr"
+#SBATCH --account=che240027p
+
+module reset
+module load intelmpi/2021.3.0-intel2021.3.0 gcc/10.2.0 fftw 
+
+lammps=/ocean/projects/che240027p/shared/software/Team_UCSD/lammps_exe/lmp_mpi_mbx
+
+export OMP_NUM_THREADS=1
+mpirun -n 8 $lammps -in lmp_rerun.in -log log > out 2> error
