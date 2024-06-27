0b0a5949a1038b8e965cad16fb42ebae03adf27a
24ssuman <24.sumansaha@gmail.com>
2024-06-27 15:42:20 -0400

    updated ion_water traj

--- ion_water/analyse_equilibriated_traj/sub.sh
+++ ion_water/analyse_equilibriated_traj/sub.sh
@@ -1,19 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-#SBATCH --job-name="mbpol_ion"
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8 
-#SBATCH --cpus-per-task=1
-#SBATCH --partition=RM-small
-#SBATCH --time=00:20:00
+#SBATCH --job-name="run1"
 #SBATCH --output="stdout"
 #SBATCH --error="stderr"
-#SBATCH --account=che240027p
+#SBATCH --partition=shared
+##SBATCH --partition=compute
+##SBATCH --partition=debug
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+##SBATCH --mem=64GB
+#SBATCH -A csd808
+#SBATCH --export=ALL
+#SBATCH -t 24:00:00
 
-module reset
-module load intelmpi/2021.3.0-intel2021.3.0 gcc/10.2.0 fftw 
+# Load modules
+module load cpu/0.15.4  gcc/9.2.0  gsl/2.5  openmpi/3.1.6 fftw/3.3.8 python/3.8.5 py-numpy/1.19.2-openblas
 
-lammps=/ocean/projects/che240027p/shared/software/Team_UCSD/lammps_exe/lmp_mpi_mbx
+cd $SLURM_SUBMIT_DIR
+lammps=/home/s7saha/software/MBX_updated_lmp_plumed/bin/lmp_mbx_noplumed
 
 export OMP_NUM_THREADS=1
-mpirun -n 8 $lammps -in lmp_rerun.in -log log > out 2> error
+
+srun --mpi=pmi2 --cpus-per-task=$OMP_NUM_THREADS -n 32 $lammps -in lmp.in -log log > out 2> error
